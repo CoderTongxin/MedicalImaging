fe47fe2b7d00d0f995083635a707bcc304943abe
CoderTongxin <gin199413@163.com>
2018-09-06 16:57:45 +1200

    add google auth to home page

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const stylus = require('stylus');
 
+
+const loginRouter=require('./routes/login');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login',loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
