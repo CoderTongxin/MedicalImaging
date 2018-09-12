97f1b258575603f9ef3541eb28599472c55ce2eb
CoderTongxin <gin199413@163.com>
2018-09-12 16:39:18 +1200

    delete login page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const stylus = require('stylus');
 
 
-const loginRouter=require('./routes/login');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login',loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
