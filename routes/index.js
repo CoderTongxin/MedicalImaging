0d1ae6271e69c008a1d4c68096b078a6f5baf769
CoderTongxin <gin199413@163.com>
2018-09-18 16:59:06 +1200

    refactor path name

--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,26 @@ router.get('/clinicalLogBook', function(req, res, next) {
 });
 
 
-router.get('/clinicalExaminations', function(req, res, next) {
-    res.render('clinicalExaminations');
+router.get('/clinicalExaminationsTotal', function(req, res, next) {
+    res.render('clinicalExaminations',{type:'total'});
 });
 
-router.get('/clinicalHours', function(req, res, next) {
-    res.render('clinicalHours');
+router.get('/clinicalExaminationsCategory', function(req, res, next) {
+    res.render('clinicalExaminations',{type:'category'});
+});
+
+router.get('/clinicalExaminationsSubcategory', function(req, res, next) {
+    res.render('clinicalExaminations',{type:'subcategory'});
+});
+
+router.get('/clinicalHoursTotal', function(req, res, next) {
+    res.render('clinicalHours',{type:'total'});
+});
+router.get('/clinicalHoursMonth', function(req, res, next) {
+    res.render('clinicalHours',{type:'month'});
+});
+router.get('/clinicalHoursWeek', function(req, res, next) {
+    res.render('clinicalHours',{type:'week'});
 });
 
 router.get('/clinicalSummary', function(req, res, next) {
